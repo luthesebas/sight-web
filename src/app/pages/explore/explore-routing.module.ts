c43081f1010b014635ebd7f025b08fb8573ce501
luthesebas <luthesebas@gmail.com>
2019-03-12 23:09:07 +0100

    Bind /explore to ExploreViewComponent

--- src/app/pages/explore/explore-routing.module.ts
+++ src/app/pages/explore/explore-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ExploreViewComponent } from './explore-view/explore-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'explore', component: ExploreViewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
