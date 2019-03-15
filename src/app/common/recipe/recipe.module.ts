c496bfc1582ed775c7333b4bfe41e673fd373a63
luthesebas <luthesebas@gmail.com>
2019-03-15 22:08:35 +0100

    Create recipe-list-item.component

--- src/app/common/recipe/recipe.module.ts
+++ src/app/common/recipe/recipe.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RecipeListItemComponent } from './recipe-list-item/recipe-list-item.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [RecipeListItemComponent],
   imports: [
     CommonModule
   ]
