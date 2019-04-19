09e4e1fa81c3e887a7969e40a1246e7c077da343
luthesebas <luthesebas@gmail.com>
2019-04-19 11:17:50 +0200

    Update mock data

--- src/app/pages/explore/explore-view/explore-view.component.ts
+++ src/app/pages/explore/explore-view/explore-view.component.ts
@@ -17,16 +17,22 @@ export class ExploreViewComponent implements OnInit {
     recipe0.id = 0;
     recipe0.details.title = 'Title 0';
     recipe0.details.description = 'Description of recipe 0';
+    recipe0.rating.averageScore = 3;
+    recipe0.rating.totalRatings = 7;
     recipe0.imageSources.push('assets/404.png');
     const recipe1 = new Recipe();
     recipe1.id = 1;
     recipe1.details.title = 'Title 1';
     recipe1.details.description = 'Description of recipe 1';
+    recipe1.rating.averageScore = 2.6;
+    recipe1.rating.totalRatings = 12;
     recipe1.imageSources.push('assets/404.png');
     const recipe2 = new Recipe();
     recipe2.id = 2;
     recipe2.details.title = 'Title 2';
     recipe2.details.description = 'Description of recipe 2';
+    recipe2.rating.averageScore = 3.9;
+    recipe2.rating.totalRatings = 10;
     recipe2.imageSources.push('assets/404.png');
 
     this.recipes = [recipe0, recipe1, recipe2];
