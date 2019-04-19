ae4408b68ec1660d5882c0cff0f687430d3dec67
luthesebas <luthesebas@gmail.com>
2019-04-19 11:48:51 +0200

    Update recipe list item view

--- src/app/shared/components/recipe/recipe.module.ts
+++ src/app/shared/components/recipe/recipe.module.ts
@@ -3,13 +3,19 @@ import { CommonModule } from '@angular/common';
 import { RecipeListItemComponent } from './recipe-list-item/recipe-list-item.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
   declarations: [
     RecipeListItemComponent,
     RecipeListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+
+    MatCardModule,
+    MatButtonModule
   ],
   exports: [
     RecipeListComponent
