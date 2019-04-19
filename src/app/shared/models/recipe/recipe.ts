b26a332d330e32af61888bb6b2a3e3afcf06caa6
luthesebas <luthesebas@gmail.com>
2019-04-19 10:33:16 +0200

    Update recipe.ts

--- src/app/shared/models/recipe/recipe.ts
+++ src/app/shared/models/recipe/recipe.ts
@@ -1,2 +1,19 @@
+import { Rating } from '../rating';
+import { RecipeDetails } from './recipe-details';
+
 export class Recipe {
+
+  id: number;
+  details: RecipeDetails;
+  rating = new Rating();
+  imageSources: string[];
+
+  public getCoverImageSource(): string {
+    if (this.imageSources && this.imageSources.length >= 0) {
+      return this.imageSources[0];
+    } else {
+      return undefined;
+    }
+  }
+
 }
