f37abd9b8c30ab67608af1efa8876ea75db95913
luthesebas <luthesebas@gmail.com>
2019-04-30 16:27:02 +0200

    Create rating.module.ts

---
+++ src/app/shared/components/rating/rating.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RatingComponent } from './rating.component';
+
+import { MatIconModule } from '@angular/material/icon';
+
+@NgModule({
+  declarations: [
+    RatingComponent
+  ],
+  imports: [
+    CommonModule,
+
+    // Angular Material
+    MatIconModule
+  ],
+  exports: [
+    RatingComponent
+  ]
+})
+export class RatingModule { }
