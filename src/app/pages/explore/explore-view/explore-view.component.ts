3f080f4814e6dda433a05978a0c8ec6bf9e07562
luthesebas <luthesebas@gmail.com>
2019-05-03 10:51:20 +0200

    Update example recipe pic

--- src/app/pages/explore/explore-view/explore-view.component.ts
+++ src/app/pages/explore/explore-view/explore-view.component.ts
@@ -19,21 +19,21 @@ export class ExploreViewComponent implements OnInit {
     recipe0.details.description = 'Festliches, mediterranes Fischgericht';
     recipe0.rating.averageScore = 4.59;
     recipe0.rating.totalRatings = 20;
-    recipe0.imageSources.push('assets/404.png');
+    recipe0.imageSources.push('assets/example.JPG');
     const recipe1 = new Recipe();
     recipe1.id = 1;
     recipe1.details.title = 'Kaiserschmarrn';
     recipe1.details.description = '';
     recipe1.rating.averageScore = 4.3;
     recipe1.rating.totalRatings = 21;
-    recipe1.imageSources.push('assets/404.png');
+    recipe1.imageSources.push('assets/example.JPG');
     const recipe2 = new Recipe();
     recipe2.id = 2;
     recipe2.details.title = 'Geschnetzeltes in Estragon-Wein-Sauce mit Limette';
     recipe2.details.description = 'Geschnetzeltes aus Schweinefilet und Rinderhüfte mit Reis';
     recipe2.rating.averageScore = 4.26;
     recipe2.rating.totalRatings = 17;
-    recipe2.imageSources.push('assets/404.png');
+    recipe2.imageSources.push('assets/example.JPG');
 
     this.recipes = [recipe0, recipe1, recipe2];
   }
