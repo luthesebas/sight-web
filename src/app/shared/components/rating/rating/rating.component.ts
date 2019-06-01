0aab8d8d506141aab48804d2ff460a52a822ed08
luthesebas <luthesebas@gmail.com>
2019-06-01 17:32:37 +0200

    Update rating.component.ts

--- src/app/shared/components/rating/rating/rating.component.ts
+++ src/app/shared/components/rating/rating/rating.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+
 import { Rating } from '../../../models/rating';
 
 @Component({
@@ -18,12 +19,11 @@ export class RatingComponent implements OnInit {
   public getIconName(index: number): string {
     if (this.rating.averageScore >= index) {
       return 'star';
-    } else {
-      if (this.rating.averageScore >= index - 0.45) {
-        return 'star_half';
-      }
-      return 'star_border';
     }
+    if (this.rating.averageScore >= index - 0.45) {
+      return 'star_half';
+    }
+    return 'star_border';
   }
 
 }
