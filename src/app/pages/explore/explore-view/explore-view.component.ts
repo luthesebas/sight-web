13aa540f9afb938d02f27d9cbf90c5edfb53194c
luthesebas <luthesebas@gmail.com>
2019-06-01 17:33:03 +0200

    Update explore-view.component.ts

--- src/app/pages/explore/explore-view/explore-view.component.ts
+++ src/app/pages/explore/explore-view/explore-view.component.ts
@@ -1,20 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
 import { Recipe } from 'src/app/shared/models/recipe/recipe';
 import { ExploreService } from '../service/explore.service';
+
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-explore-view',
   templateUrl: './explore-view.component.html',
   styleUrls: ['./explore-view.component.scss']
 })
-export class ExploreViewComponent implements OnInit {
+export class ExploreViewComponent implements OnInit, OnDestroy {
 
-  constructor(private exploreService: ExploreService) { }
+  constructor(
+    private exploreService: ExploreService
+  ) { }
 
+  private destroyed$ = new Subject();
   public recipes: Recipe[];
 
   ngOnInit() {
-    this.recipes = this.exploreService.getRecipes();
+    this.exploreService.getRecipes()
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe(items => this.recipes = items);
+  }
+
+  ngOnDestroy() {
+    this.destroyed$.next();
+    this.destroyed$.complete();
   }
 
 }
