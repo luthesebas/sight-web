c1257d8dcbfd207a22fbe93915cef5d4266cc2f5
luthesebas <luthesebas@gmail.com>
2019-07-06 17:39:34 +0200

    Update changeDetection to OnPush

--- src/app/shared/components/recipe/recipe-list-item/recipe-list-item.component.ts
+++ src/app/shared/components/recipe/recipe-list-item/recipe-list-item.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 
 import { Recipe } from 'src/app/shared/models/recipe/recipe';
 
 @Component({
   selector: 'app-recipe-list-item',
   templateUrl: './recipe-list-item.component.html',
-  styleUrls: ['./recipe-list-item.component.scss']
+  styleUrls: ['./recipe-list-item.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RecipeListItemComponent implements OnInit {
 
@@ -17,8 +18,7 @@ export class RecipeListItemComponent implements OnInit {
   ngOnInit() { }
 
   public openDetailsOf(recipe: Recipe): void {
-    // TODO Open recipe details
-    console.log('[TODO] Open recipe details');
+    // TODO
   }
 
 }
--- src/app/shared/components/recipe/recipe-list/recipe-list.component.ts
+++ src/app/shared/components/recipe/recipe-list/recipe-list.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 
 import { Recipe } from 'src/app/shared/models/recipe/recipe';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls: ['./recipe-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RecipeListComponent implements OnInit {
 
