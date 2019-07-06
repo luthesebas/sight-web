73e279a76cceb894f8a9fa00a9eb562aa309ec2f
luthesebas <luthesebas@gmail.com>
2019-07-06 20:23:51 +0200

    Create recipe-details

---
+++ src/app/shared/components/recipe/recipe-details/recipe-details.component.ts
@@ -0,0 +1,25 @@
+import {
+  Component,
+  OnInit,
+  Input,
+  ChangeDetectionStrategy
+} from '@angular/core';
+import { Recipe } from 'src/app/shared/models/recipe/recipe';
+
+@Component({
+  selector: 'app-recipe-details',
+  templateUrl: './recipe-details.component.html',
+  styleUrls: ['./recipe-details.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class RecipeDetailsComponent implements OnInit {
+
+  @Input()
+  public recipe: Recipe;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
