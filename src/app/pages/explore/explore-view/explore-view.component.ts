be19b655ff5f7c44b44fca09223d8090ad34f189
luthesebas <luthesebas@gmail.com>
2019-07-06 17:49:52 +0200

    Use NgRx in ExploreView

--- src/app/pages/explore/explore-view/explore-view.component.ts
+++ src/app/pages/explore/explore-view/explore-view.component.ts
@@ -1,34 +1,29 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { State } from '../redux/reducers/recipe.reducer';
+import { getRecipesLoading, getRecipes } from '../redux/selectors/recipe.selectors';
+import { LoadRecipes } from '../redux/actions/recipe.actions';
 import { Recipe } from 'src/app/shared/models/recipe/recipe';
-import { ExploreService } from '../service/explore.service';
-
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-explore-view',
   templateUrl: './explore-view.component.html',
   styleUrls: ['./explore-view.component.scss']
 })
-export class ExploreViewComponent implements OnInit, OnDestroy {
+export class ExploreViewComponent implements OnInit {
+
+  loading$: Observable<boolean> = this.store.select(getRecipesLoading);
+  recipes$: Observable<Recipe[]> = this.store.select(getRecipes);
 
   constructor(
-    private exploreService: ExploreService
+    private store: Store<State>
   ) { }
 
-  private destroyed$ = new Subject();
-  public recipes: Recipe[];
-
   ngOnInit() {
-    this.exploreService.getRecipes()
-      .pipe(takeUntil(this.destroyed$))
-      .subscribe(items => this.recipes = items);
-  }
-
-  ngOnDestroy() {
-    this.destroyed$.next();
-    this.destroyed$.complete();
+    this.store.dispatch(new LoadRecipes());
   }
 
 }
