fcfa2b7fe6d42e2e39f699d03194c0fd360c4398
luthesebas <luthesebas@gmail.com>
2019-07-06 17:42:00 +0200

    Add NgRx to ExploreModule

--- src/app/pages/explore/explore.module.ts
+++ src/app/pages/explore/explore.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+
 import { ExploreRoutingModule } from './explore-routing.module';
 import { ExploreViewComponent } from './explore-view/explore-view.component';
 import { RecipeModule } from 'src/app/shared/components/recipe/recipe.module';
 import { ExploreService } from './service/explore.service';
+import * as fromRecipe from './redux/reducers/recipe.reducer';
+import { RecipeEffects } from './redux/effects/recipe.effects';
 
 @NgModule({
   declarations: [
@@ -13,7 +18,9 @@ import { ExploreService } from './service/explore.service';
   imports: [
     CommonModule,
     RecipeModule,
-    ExploreRoutingModule
+    ExploreRoutingModule,
+    StoreModule.forFeature('recipe', fromRecipe.reducer),
+    EffectsModule.forFeature([RecipeEffects])
   ],
   providers: [
     ExploreService
