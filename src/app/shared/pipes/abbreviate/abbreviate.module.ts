4509186cd184a0c2bb37c8e6977ef53368ec5f8a
luthesebas <luthesebas@gmail.com>
2020-11-21 11:14:10 +0100

    Create AbbreviateModule

---
+++ src/app/shared/pipes/abbreviate/abbreviate.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AbbreviatePipe } from './abbreviate.pipe';
+
+@NgModule({
+  declarations: [
+    AbbreviatePipe
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    AbbreviatePipe
+  ]
+})
+export class AbbreviateModule { }
