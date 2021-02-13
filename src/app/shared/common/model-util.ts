6a0403318ddcd535766d8b93fce74d820a25722a
luthesebas <luthesebas@gmail.com>
2021-02-13 10:58:41 +0100

    Create model-util.ts

---
+++ src/app/shared/common/model-util.ts
@@ -0,0 +1,29 @@
+export type Comparator<T> = (x: T, y: T) => number;
+
+export type Equalizer<T> = (x: T, y: T) => boolean;
+
+//export type Finder<T> = ((x: T, y: T[]) => T | null) & ((x: T[], y: T[]) => T[]);
+
+export type Combiner<M, N, R> = (x: M, y: N) => R;
+
+
+export function createComparator<T>(baselineCompare: Comparator<T>): Comparator<T> {
+  return (x: T, y: T) => {
+      if (x === y || x == y) {
+          return 0;
+      }
+      if (x == null) {
+          return -1;
+      }
+      if (y == null) {
+          return 1;
+      }
+      return baselineCompare(x, y);
+  }
+}
+
+export function createEqualizer<T>(baselineEquals: Equalizer<T>): Equalizer<T> {
+  return (x: T, y: T) => {
+      return (x === y) || ((x != null && y != null) && baselineEquals(x, y));
+  }
+}
