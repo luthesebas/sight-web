c85ac29e18de479829e0c31d99dc93a0c3ddcee8
luthesebas <luthesebas@gmail.com>
2021-02-13 11:03:02 +0100

    Create MediaService

---
+++ src/app/shared/media/services/media.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MediaService {
+
+  isMobileDevice$: Observable<boolean>;
+  isStationaryDevice$: Observable<boolean>;
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+  ) {
+    this.isMobileDevice$ = this.breakpointObserver
+      .observe([Breakpoints.Handset, Breakpoints.Tablet])
+      .pipe(
+        map(result => result.matches),
+        shareReplay({ refCount: true }),
+      );
+
+    this.isStationaryDevice$ = this.breakpointObserver
+      .observe([Breakpoints.Web])
+      .pipe(
+        map(result => result.matches),
+        shareReplay({ refCount: true }),
+      );
+  }
+
+}
